ae7d085bdaf011ba757ed94b19835c119fb259e1
Mansaaaa <aiman.salehudin@live.iium.edu.my>
2025-01-11 22:51:43 +0800

    test ICOProject.java

--- ICOProject.java
+++ ICOProject.java
@@ -1,8 +1,28 @@
-
+// ICO PROJECT
+// SIMPLE CASHER SYSTEM
 package RandomClass;
+import java.util.Scanner;
 
 public class ICOProject {
     public static void main(String[] args) {
+        // MAIN METHOD
+        // PRODUCT ARRAY SHOULD BE STATED HERE AND PASS ONTO OTHER METHODS
+
+        //-----------------PRODUCT DATA------------------------ faisal space
+
+
+        //-----------------PRODUCT DATA------------------------
+
+        greetings();
+        while(true){
+            query();
+        }
         
     }
+
+    public static void greetings(){
+        System.out.println("
+            }
+
+    
 }
