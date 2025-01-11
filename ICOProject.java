18b1f21f6a9d9e273445e9f4501a0554b5ba155f
Mansaaaa <aiman.salehudin@live.iium.edu.my>
2025-01-12 00:25:33 +0800

    Update ICOProject.java

--- ICOProject.java
+++ ICOProject.java
@@ -13,16 +13,32 @@ public class ICOProject {
 
         //-----------------PRODUCT DATA------------------------
 
-        greetings();
+        greetings(prodid,prodname,price);
         while(true){
             query();
         }
         
     }
 
-    public static void greetings(){
-        System.out.println("
-            }
+    public static void greetings(int[] prodid, String[] prodname, double[] price){
+        System.out.println("/////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+        System.out.println("            Welcome to Casher System                 ");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\//////////////////////////\n");
+
+        System.out.println("Here are the products we sell:");
+        System.out.println("Product ID    Product Name    Price");
+        System.out.println("------------------------------------");
+        for (int i = 0 ; i < prodid.length ; i++){
+            System.out.printf("%-10d    %-12s    %-.25f", prodid[i],prodname[i],price[i]);
+    }
+
+    public static double totalPrice(List<Double> prices){ 
+        double totalPrice = 0.0; 
+        for(double price:prices){ 
+            totalPrice+=price; 
+        } 
+        return totalPrice; 
+    } //Taufiq 
 
     
 }
